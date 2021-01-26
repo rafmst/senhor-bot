a1dd18acfeec86c64ed67f76545d2ccf50a0376a
Rafael Morais <rafaelcanical@gmail.com>
2021-01-26 19:38:02 +0100

    Rework !weather to work with the database

--- commands/weather.go
+++ commands/weather.go
@@ -1,12 +1,17 @@
 package commands
 
 import (
+	"context"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 
 	owm "github.com/briandowns/openweathermap"
 	"github.com/bwmarrin/discordgo"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // HandleWeather handles "!weather" command
@@ -28,36 +33,62 @@ func HandleWeather(session *discordgo.Session, command *discordgo.MessageCreate)
 	location = strings.TrimPrefix(location, " ")
 
 	if len(location) > 0 {
-		// Connect to weather api
-		apiKey := os.Getenv("WEATHER_API_KEY")
-		weather, err := owm.NewCurrent("C", "pt", apiKey)
-
-		// Check for connection error
-		if err != nil {
-			session.ChannelMessageSend(command.ChannelID, "Error: Connection to Weather API failed")
-		}
-
-		weather.CurrentByName(location)
-
-		thumbnail := discordgo.MessageEmbedThumbnail{
-			URL: "http://openweathermap.org/img/wn/" + weather.Weather[0].Icon + "@2x.png",
-		}
-
-		current := strconv.Itoa(int(weather.Main.Temp))
-		max := strconv.Itoa(int(weather.Main.TempMax))
-		min := strconv.Itoa(int(weather.Main.TempMin))
-
-		var message = discordgo.MessageEmbed{
-			Title: weather.Name,
-			Description: `Temperatura actual: **` + current + `°C** 
-			Máximas de: **` + max + `°C**
-			Mínimas de: **` + min + `°C**`,
-			Thumbnail: &thumbnail,
-		}
-
-		session.ChannelMessageSendEmbed(command.ChannelID, &message)
+		getWeatherInfo(location, session, command)
 	} else {
-		session.ChannelMessageSend(command.ChannelID, "Escolha uma localizacão, exemplo: `"+usedPrefix+" Mafamude`")
+		client, err := mongo.NewClient(options.Client().ApplyURI(os.Getenv("DATABASE")))
+		if err != nil {
+			session.ChannelMessageSend(command.ChannelID, "Error: Couldn't connect to Mongo Database")
+		} else {
+			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+			err = client.Connect(ctx)
+			if err != nil {
+				session.ChannelMessageSend(command.ChannelID, "Error: Couldn't establish connection to Mongo Database")
+			} else {
+				var user User
+				db := client.Database("senhor-bot")
+				users := db.Collection("users")
+
+				filter := bson.M{"discord_id": command.Author.ID}
+				err = users.FindOne(ctx, filter).Decode(&user)
+
+				if err != nil {
+					session.ChannelMessageSend(command.ChannelID, "Escolha uma localizacão, exemplo: `"+usedPrefix+" Mafamude`")
+				} else {
+					getWeatherInfo(user.City, session, command)
+				}
+			}
+			cancel()
+		}
+	}
+}
+
+func getWeatherInfo(location string, session *discordgo.Session, command *discordgo.MessageCreate) {
+	// Connect to weather api
+	apiKey := os.Getenv("WEATHER_API_KEY")
+	weather, err := owm.NewCurrent("C", "pt", apiKey)
+
+	// Check for connection error
+	if err != nil {
+		session.ChannelMessageSend(command.ChannelID, "Error: Connection to Weather API failed")
 	}
 
+	weather.CurrentByName(location)
+
+	thumbnail := discordgo.MessageEmbedThumbnail{
+		URL: "http://openweathermap.org/img/wn/" + weather.Weather[0].Icon + "@2x.png",
+	}
+
+	current := strconv.Itoa(int(weather.Main.Temp))
+	max := strconv.Itoa(int(weather.Main.TempMax))
+	min := strconv.Itoa(int(weather.Main.TempMin))
+
+	var message = discordgo.MessageEmbed{
+		Title: weather.Name,
+		Description: `Temperatura actual: **` + current + `°C** 
+	Máximas de: **` + max + `°C**
+	Mínimas de: **` + min + `°C**`,
+		Thumbnail: &thumbnail,
+	}
+
+	session.ChannelMessageSendEmbed(command.ChannelID, &message)
 }
